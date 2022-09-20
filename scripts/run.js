4d3a6b21b3087e0cb24b90e97c2bb07c5fb2c505
Maksym Bielushkin <maxb@getblend.com>
2022-09-20 18:33:58 +0200

    Removed and added files

--- scripts/run.js
+++ scripts/run.js
@@ -1,35 +1,43 @@
 const main = async () => {
-  const [owner, randomPerson] = await hre.ethers.getSigners();
-  const contractFactory = await hre.ethers.getContractFactory("Portal");
-  const contract = await contractFactory.deploy();
-  await contract.deployed();
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.1"),
+  });
+  await waveContract.deployed();
+  console.log("Contract addy:", waveContract.address);
 
-  console.log("Contract deployed to:", contract.address);
-  console.log("Contract deployed by:", owner.address);
-  
-  let waveCount;
-  waveCount = await contract.getTotalWaves();
+  let contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
+  console.log(
+    "Contract balance:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
 
-  let waveTxn = await contract.wave();
+  const waveTxn = await waveContract.wave("This is wave #1");
   await waveTxn.wait();
 
-  waveCount = await contract.getTotalWaves();
-  
-  waveTxn = await contract.connect(randomPerson).wave();
-  await waveTxn.wait();
+  const waveTxn2 = await waveContract.wave("This is wave #2");
+  await waveTxn2.wait();
 
-  waveCount = await contract.getTotalWaves();
+  contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+  console.log(
+    "Contract balance:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
 };
 
 const runMain = async () => {
   try {
     await main();
-    process.exit(0); // exit Node process without error
+    process.exit(0);
   } catch (error) {
     console.log(error);
-    process.exit(1); // exit Node process while indicating 'Uncaught Fatal Exception' error
+    process.exit(1);
   }
-  // Read more about Node exit ('process.exit(num)') status codes here: https://stackoverflow.com/a/47163396/7974948
 };
 
 runMain();
